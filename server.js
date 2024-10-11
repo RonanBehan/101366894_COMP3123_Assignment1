120e702811a8fc8bba4703364157a583e66414fd
RonanBehan <144288415+RonanBehan@users.noreply.github.com>
2024-10-11 01:32:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.use(express.json());
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/emp', employeeRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
